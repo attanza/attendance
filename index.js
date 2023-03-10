b2adff18b31a1882252623adca197ea6564f9950
Dani Lesmiadi <dani.lesmiadi@pegadaian.co.id>
2023-03-10 09:00:59 +0700

    hardening added

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 import express from 'express';
+import rateLimit from 'express-rate-limit';
+import helmet from 'helmet';
 import 'dotenv/config';
 import { checkInOffice } from './libs/checkInOffice.js';
 import { workDay } from './libs/workDay.js';
 
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minutes
+  max: 10, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
 const app = express();
 const port = 10000;
+app.disable('x-powered-by');
+app.use(helmet());
+app.use(limiter);
 
 app.get('/', (req, res) => res.send('Hello World!'));
 app.post('/attendance', async (req, res) => {
@@ -19,6 +31,17 @@ app.post('/attendance', async (req, res) => {
     console.log(error);
     res.status(500).send(error);
   }
+});
+
+// custom 404
+app.use((req, res, next) => {
+  res.status(404).send("Sorry can't find that!");
+});
+
+// custom error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
 });
 app.listen(port, () =>
   console.log(`Example app listening on http://localhost:${port}`)
